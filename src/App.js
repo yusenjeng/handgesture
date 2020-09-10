7b16ea971ac91c813b7069ab90f5708cdeb98fa0
Zixi Xu <zixixu2017@gmail.com>
2020-09-10 14:13:40 -0700

    minor refine

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import '@tensorflow/tfjs-backend-webgl';
 import '@tensorflow/tfjs-backend-cpu';
 
 let model;
+let iterator = 0;
 
 function App() {
   const localVideo = useRef(null);
@@ -12,9 +13,10 @@ function App() {
   useEffect(()=>{
     async function setup(){
       await tf.setBackend('webgl');
-      console.log(new Date());
+
+      const startLoadTime = new Date().getTime();
       model = await handtrack.load();
-      console.log(new Date());
+      console.log("ML model loaded. Elapsed time: ", new Date().getTime() - startLoadTime);
       
       const stream = await navigator.mediaDevices.getUserMedia({video:true});
       console.log("Get user media success!", stream);
@@ -22,7 +24,7 @@ function App() {
       // for react
       localVideo.current.srcObject = stream;
 
-      // for ML model
+      // for ML model, has to be HTMLVideoElement
       let videoElem = document.createElement("video");
       videoElem.srcObject = stream;
 
@@ -36,65 +38,58 @@ function App() {
     async function main() {
       const localVideoElem = await setup();
       localVideoElem.play();
+      console.log("Loaded data, start computing...");
 
-      // TODO: use hardcoded 3s interval
+      // TODO: now only use hardcoded interval
       setInterval(
         async () => {await computeHandpose(localVideoElem)}, 
-        3000
+        2000
       );
     }
 
     const computeHandpose = async function(videoElem) {
-      console.log("Loaded data, start computing...");
-      let i = 0;
-      // while (i < 100) {
-        const predictions = await model.estimateHands(videoElem);
-        // console.log("Finished: ", predictions);
-        // console.log(localVideoElem.srcObject);
-        console.log("Iteration: ", i);
-        if (predictions.length > 0) {
-          /*
-          `predictions` is an array of objects describing each detected hand, for example:
-          [
-            {
-              handInViewConfidence: 1, // The probability of a hand being present.
-              boundingBox: { // The bounding box surrounding the hand.
-                topLeft: [162.91, -17.42],
-                bottomRight: [548.56, 368.23],
-              },
-              landmarks: [ // The 3D coordinates of each hand landmark.
-                [472.52, 298.59, 0.00],
-                [412.80, 315.64, -6.18],
+      const predictions = await model.estimateHands(videoElem);
+      console.log("Iteration: ", iterator++, ", predictions length: ", predictions.length);
+      if (predictions.length > 0) {
+        /*
+        `predictions` is an array of objects describing each detected hand, for example:
+        [
+          {
+            handInViewConfidence: 1, // The probability of a hand being present.
+            boundingBox: { // The bounding box surrounding the hand.
+              topLeft: [162.91, -17.42],
+              bottomRight: [548.56, 368.23],
+            },
+            landmarks: [ // The 3D coordinates of each hand landmark.
+              [472.52, 298.59, 0.00],
+              [412.80, 315.64, -6.18],
+              ...
+            ],
+            annotations: { // Semantic groupings of the `landmarks` coordinates.
+              thumb: [
+                [412.80, 315.64, -6.18]
+                [350.02, 298.38, -7.14],
                 ...
               ],
-              annotations: { // Semantic groupings of the `landmarks` coordinates.
-                thumb: [
-                  [412.80, 315.64, -6.18]
-                  [350.02, 298.38, -7.14],
-                  ...
-                ],
-                ...
-              }
-            }
-          ]
-          */
-  
-          for (let i = 0; i < predictions.length; i++) {
-            const keypoints = predictions[i].landmarks;
-  
-            // Log hand keypoints.
-            for (let i = 0; i < keypoints.length; i++) {
-              const [x, y, z] = keypoints[i];
-              console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+              ...
             }
           }
+        ]
+        */
+
+        for (let i = 0; i < predictions.length; i++) {
+          const keypoints = predictions[i].landmarks;
+
+          // Log hand keypoints.
+          for (let i = 0; i < keypoints.length; i++) {
+            const [x, y, z] = keypoints[i];
+            console.log(`Keypoint ${i}: [${x}, ${y}, ${z}]`);
+          }
         }
-        // i++;
-      // }
+      }
     }
     
     main();
-
   }, []);
 
   return (
