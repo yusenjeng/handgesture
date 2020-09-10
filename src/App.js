dec527a9486793f89ee656ae4433386b99bf1753
yjeng <yjeng@cisco.com>
2020-09-10 10:59:35 -0700

    debug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import * as handtrack from '@tensorflow-models/handpose';
 
 function App() {
+
+  useEffect(()=>{
+    async function init(){
+      console.log(new Date());
+      const model = await handtrack.load();
+      console.log(new Date());
+    }
+
+    init();
+
+  }, []);
+
   return (
     <div className="App">
       yes we can
